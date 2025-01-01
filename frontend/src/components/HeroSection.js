71a43c5ca4fc66ed401492ab294d1176778e6162
dingle17 <kylerdingle15@gmail.com>
2025-01-02 07:23:43 +0800

    latest

--- frontend/src/components/HeroSection.js
+++ frontend/src/components/HeroSection.js
@@ -1,185 +1,131 @@
-import React from "react";
-import Hero from "../assets/heroSectionBg.jpg";
-import WaterContainer from "../assets/waterContainer.png";
-import WaterContainer2 from "../assets/waterContainer2.png";
-import bottel3 from "../assets/bottel3.png";
-import Ice3 from "../assets/ice3.png";
-import Ice4 from "../assets/ice4.png";
-import Ice1 from "../assets/ice1.png";
-import Ice2 from "../assets/ice2.png";
+  import React from "react";
+  import Hero from "../assets/heroSectionBg.jpg";
+  import bottel3 from "../assets/bottel3.png";
+  import Ice3 from "../assets/ice3.png";
+  import Ice4 from "../assets/ice4.png";
+  import Ice1 from "../assets/ice1.png";
+  import Ice2 from "../assets/ice2.png";
 
-const HeroSection = () => {
-  return (
-    <section
-    className="h-screen text-gray-800 bg-center w-full"
-    style={{
-      backgroundImage: `url(${Hero})`,
-      backgroundAttachment: 'fixed',
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-    }}
-  >
-      <div className="h-full flex items-center justify-center ">
-        {/* Left Section */}
-  
-<div className="text-center lg:text-left lg:w-1/2 px-8">
-  <h1 className="text-8xl font-bold text-black mb-4">
-    Refresh Your Day with Pure Water
-  </h1>
-  <p className="text-xl text-black mb-6">
-    Nature’s Best Hydration <br /> Discover the cleanest, healthiest, and most refreshing water for your body.
-  </p>
+  const HeroSection = () => {
+    return (
+      <section
+        className="h-screen text-gray-800 bg-center w-full"
+        style={{
+          backgroundImage: `url(${Hero})`,
+          backgroundAttachment: "fixed",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        {/* 1st hero section */}
+        <div className="h-full flex flex-col lg:flex-row items-center justify-center px-6 lg:px-16">
+          {/* Left Section */}
+          <div className="text-center lg:text-left lg:w-1/2">
+            <h1 className="text-4xl md:text-6xl lg:text-8xl font-bold text-black mb-4">
+              Refresh Your Day with Pure Water
+            </h1>
+            <p className="text-lg md:text-xl lg:text-2xl text-black mb-6">
+              Nature’s Best Hydration <br /> Discover the cleanest, healthiest,
+              and most refreshing water for your body.
+            </p>
 
-  <div className="flex flex-wrap justify-center lg:justify-start space-x-4">
-    <button className="bg-blue-600 hover:bg-blue-500 text-white px-6 py-3 rounded-lg shadow-lg">
-      More About Us
-    </button>
-    <button className="bg-white hover:bg-gray-200 text-blue-600 px-6 py-3 rounded-lg shadow-lg">
-      Contact Us
-    </button>
+            <div className="flex flex-wrap justify-center lg:justify-start space-x-4">
+              <button className="bg-blue-600 hover:bg-blue-500 text-white px-4 py-2 md:px-6 md:py-3 rounded-lg shadow-lg">
+                More About Us
+              </button>
+              <button className="bg-white hover:bg-gray-200 text-blue-600 px-4 py-2 md:px-6 md:py-3 rounded-lg shadow-lg">
+                Contact Us
+              </button>
+            </div>
+          </div>
+
+          {/* Right Section (Hidden on Small Screens) */}
+          <div className="relative h-auto hidden md:block">
+    <img
+      src={Ice1}
+      alt="Ice"
+      className="absolute bottom-8 -left-24 h-8 md:h-20 lg:h-48 w-auto z-0"
+    />
+    <img
+      src={Ice2}
+      alt="Ice"
+      className="absolute bottom-8 -right-24 h-8 md:h-20 lg:h-48 w-auto z-0"
+    />
+    <img
+      src={bottel3}
+      alt="Rotated Water Container"
+      className="relative h-auto w-auto z-1"
+    />
+    <img
+      src={Ice4}
+      alt="Ice"
+      className="absolute -bottom-6 right-16 h-8 md:h-20 lg:h-32 w-auto z-10"
+    />
+    <img
+      src={Ice3}
+      alt="Ice"
+      className="absolute -bottom-6 left-16 h-8 md:h-20 lg:h-32 w-auto z-10"
+    />
   </div>
-</div>
-
-
-        {/* Right Section */}
-
-        <div className="relative h-auto">
-        <img
-            src={Ice1}
-            alt="Ice"
-            className="absolute bottom-8 -left-24 h-8 md:h-20 lg:h-48 w-auto z-0"
-          />
-        <img
-            src={Ice2}
-            alt="Ice"
-            className="absolute bottom-8 -right-24 h-8 md:h-20 lg:h-48 w-auto z-0"
-          />
-          <img
-            src={bottel3}
-            alt="Rotated Water Container"
-            className="relative h-auto w-auto  z-1"
-          />
-
-
-          <img
-            src={Ice4}
-            alt="Ice"
-            className="absolute -bottom-6 right-16 h-8 md:h-20 lg:h-32 w-auto z-10"
-          />
-            <img
-            src={Ice3}
-            alt="Ice"
-            className="absolute -bottom-6 left-16 h-8 md:h-20 lg:h-32 w-auto z-10"
-          />
-
         </div>
-      </div>  <div className="h-full flex items-center justify-center ">
-        {/* Left Section */}
-        <div className="text-center lg:text-left lg:w-1/2 px-8">
-          <h1 className="text-8xl font-bold text-black mb-4">
-            Natural & Pure Drinking Water
-          </h1>
-          <p className="text-xl text-black mb-6">
-            Mineral Composition <br /> Experience clean, reliable, and
-            affordable hydration solutions.
-          </p>
-          <div className="flex flex-wrap justify-center lg:justify-start space-x-4">
-            <button className="bg-blue-600 hover:bg-blue-500 text-white px-6 py-3 rounded-lg shadow-lg">
-              More About Us
-            </button>
-            <button className="bg-white hover:bg-gray-200 text-blue-600 px-6 py-3 rounded-lg shadow-lg">
-              Contact Us
-            </button>
+
+
+{/*2nd  */}
+
+<div className="h-full flex flex-col lg:flex-row items-center justify-center px-6 lg:px-16">
+          {/* Left Section */}
+          <div className="text-center lg:text-left lg:w-1/2">
+            <h1 className="text-4xl md:text-6xl lg:text-8xl font-bold text-black mb-4">
+              Refresh Your Day with Pure Water
+            </h1>
+            <p className="text-lg md:text-xl lg:text-2xl text-black mb-6">
+              Nature’s Best Hydration <br /> Discover the cleanest, healthiest,
+              and most refreshing water for your body.
+            </p>
+
+            <div className="flex flex-wrap justify-center lg:justify-start space-x-4">
+              <button className="bg-blue-600 hover:bg-blue-500 text-white px-4 py-2 md:px-6 md:py-3 rounded-lg shadow-lg">
+                More About Us
+              </button>
+              <button className="bg-white hover:bg-gray-200 text-blue-600 px-4 py-2 md:px-6 md:py-3 rounded-lg shadow-lg">
+                Contact Us
+              </button>
+            </div>
           </div>
+
+          {/* Right Section (Hidden on Small Screens) */}
+          <div className="relative h-auto hidden md:block">
+    <img
+      src={Ice1}
+      alt="Ice"
+      className="absolute bottom-8 -left-24 h-8 md:h-20 lg:h-48 w-auto z-0"
+    />
+    <img
+      src={Ice2}
+      alt="Ice"
+      className="absolute bottom-8 -right-24 h-8 md:h-20 lg:h-48 w-auto z-0"
+    />
+    <img
+      src={bottel3}
+      alt="Rotated Water Container"
+      className="relative h-auto w-auto z-1"
+    />
+    <img
+      src={Ice4}
+      alt="Ice"
+      className="absolute -bottom-6 right-16 h-8 md:h-20 lg:h-32 w-auto z-10"
+    />
+    <img
+      src={Ice3}
+      alt="Ice"
+      className="absolute -bottom-6 left-16 h-8 md:h-20 lg:h-32 w-auto z-10"
+    />
+  </div>
         </div>
 
-        {/* Right Section */}
+      
+      </section>
+    );
+  };
 
-        <div className="relative h-auto">
-        <img
-            src={Ice1}
-            alt="Ice"
-            className="absolute bottom-8 -left-24 h-8 md:h-20 lg:h-48 w-auto z-0"
-          />
-        <img
-            src={Ice2}
-            alt="Ice"
-            className="absolute bottom-8 -right-24 h-8 md:h-20 lg:h-48 w-auto z-0"
-          />
-          <img
-            src={bottel3}
-            alt="Rotated Water Container"
-            className="relative h-auto w-auto  z-1"
-          />
-
-
-          <img
-            src={Ice4}
-            alt="Ice"
-            className="absolute -bottom-6 right-16 h-8 md:h-20 lg:h-32 w-auto z-10"
-          />
-            <img
-            src={Ice3}
-            alt="Ice"
-            className="absolute -bottom-6 left-16 h-8 md:h-20 lg:h-32 w-auto z-10"
-          />
-
-        </div>
-      </div>  <div className="h-full flex items-center justify-center ">
-        {/* Left Section */}
-        <div className="text-center lg:text-left lg:w-1/2 px-8">
-          <h1 className="text-8xl font-bold text-black mb-4">
-            Natural & Pure Drinking Water
-          </h1>
-          <p className="text-xl text-black mb-6">
-            Mineral Composition <br /> Experience clean, reliable, and
-            affordable hydration solutions.
-          </p>
-          <div className="flex flex-wrap justify-center lg:justify-start space-x-4">
-            <button className="bg-blue-600 hover:bg-blue-500 text-white px-6 py-3 rounded-lg shadow-lg">
-              More About Us
-            </button>
-            <button className="bg-white hover:bg-gray-200 text-blue-600 px-6 py-3 rounded-lg shadow-lg">
-              Contact Us
-            </button>
-          </div>
-        </div>
-
-        {/* Right Section */}
-
-        <div className="relative h-auto">
-        <img
-            src={Ice1}
-            alt="Ice"
-            className="absolute bottom-8 -left-24 h-8 md:h-20 lg:h-48 w-auto z-0"
-          />
-        <img
-            src={Ice2}
-            alt="Ice"
-            className="absolute bottom-8 -right-24 h-8 md:h-20 lg:h-48 w-auto z-0"
-          />
-          <img
-            src={bottel3}
-            alt="Rotated Water Container"
-            className="relative h-auto w-auto  z-1"
-          />
-
-
-          <img
-            src={Ice4}
-            alt="Ice"
-            className="absolute -bottom-6 right-16 h-8 md:h-20 lg:h-32 w-auto z-10"
-          />
-            <img
-            src={Ice3}
-            alt="Ice"
-            className="absolute -bottom-6 left-16 h-8 md:h-20 lg:h-32 w-auto z-10"
-          />
-
-        </div>
-      </div>
-    </section>
-  );
-};
-
-export default HeroSection;
+  export default HeroSection;
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { useModal } from '../context/ModalContext'; 
+import { useModal } from '../context/ModalContext';
 import Login from './Login';
 import { useAuth } from '../context/AuthContext';
 import navlogo from '../assets/navlogo.png'
@@ -10,48 +10,62 @@ import { faRightToBracket, faShoppingCart } from '@fortawesome/free-solid-svg-ic
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const { openModal, closeModal } = useModal();
-  const { authToken, logout } = useAuth(); 
-  const [isScrolled, setIsScrolled] = useState(false); // To track scroll position
+  const { authToken, logout } = useAuth();
 
   const toggleMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 1024);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth >= 1024);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   const openLoginModal = () => {
-    openModal(<Login closeModal={closeModal} />); 
+    openModal(<Login closeModal={closeModal} />);
   };
 
   const handleLogout = () => {
-    logout();  
+    logout();
   };
 
   const isLoggedIn = Boolean(authToken);
 
-  // Track scroll position and update isScrolled state
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 50) {
-        setIsScrolled(true); // Change to solid background after 50px scroll
-      } else {
-        setIsScrolled(false); // Transparent background when at the top
-      }
-    };
 
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
 
   return (
     <nav
-      className={`fixed w-full py-3 z-30 transition-all ${isScrolled ? 'bg-white text-black' : 'bg-transparent text-black'}`}
-    >
-      <div className="w-full mx-auto px-6 flex justify-around items-center">
-      <div className="text-2xl font-bold">
-  <Link to="/">
-    <img src={navlogo} className="w-[200px] h-[60px]" alt='navlogo'/>
-  </Link>
-</div>
+    className={`fixed w-full py-2 z-30 transition-all ${
+      isLargeScreen
+        ? isScrolled
+          ? 'bg-white text-black' // Large screen on scroll
+          : 'bg-transparent text-black' // Large screen default
+        : 'bg-[#ECF2F6] text-black' // Small screen default and on scroll
+    }`}
+  >
+      <div className=" justify-between md:justify-around flex flex-wrap items-center  mx-auto p-4">
+        <div className="text-2xl font-bold">
+          <Link to="/">
+            <img src={navlogo} className="w-[200px] h-[60px]" alt='navlogo' />
+          </Link>
+        </div>
 
 
         {/* Menu */}
@@ -64,40 +78,40 @@ const Navbar = () => {
 
 
 
-        <div className="flex justify-between gap-x-6 " >
-        <div>
-    <FontAwesomeIcon
-      icon={faShoppingCart}
-      className="text-blue-600 hover:text-blue-500 text-3xl cursor-pointer"
-    />
-  </div>
+        <div className="hidden md:flex justify-between gap-x-6 " >
+          <div>
+            <FontAwesomeIcon
+              icon={faShoppingCart}
+              className="text-blue-600 hover:text-blue-500 text-3xl cursor-pointer"
+            />
+          </div>
 
-  <div className=''>
-    {!isLoggedIn ? (
-      <FontAwesomeIcon
-        icon={faRightToBracket}
-        onClick={openLoginModal}
-        className="text-blue-600 hover:text-blue-500 text-3xl cursor-pointer"
-      />
-    ) : (
-      <button
-        onClick={handleLogout}
-        className="bg-red-600 hover:bg-red-500 text-white px-4 py-2 rounded"
-      >
-        Logout
-      </button>
-    )}
-  </div>
+          <div className=''>
+            {!isLoggedIn ? (
+              <FontAwesomeIcon
+                icon={faRightToBracket}
+                onClick={openLoginModal}
+                className="text-blue-600 hover:text-blue-500 text-3xl cursor-pointer"
+              />
+            ) : (
+              <button
+                onClick={handleLogout}
+                className="bg-red-600 hover:bg-red-500 text-white px-4 py-2 rounded"
+              >
+                Logout
+              </button>
+            )}
+          </div>
 
-  {/* Cart Icon */}
- 
-</div>
+          {/* Cart Icon */}
+
+        </div>
 
 
 
         {/* Mobile Menu Button */}
-        <div className="md:hidden">
-          <button className="text-white focus:outline-none" onClick={toggleMenu}>
+        <div className="md:hidden bg-white ">
+          <button className="text-gray-500 focus:outline-none" onClick={toggleMenu}>
             <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
             </svg>
@@ -105,15 +119,37 @@ const Navbar = () => {
         </div>
       </div>
 
-      {/* Mobile Menu */}
       {isMobileMenuOpen && (
-        <div className="md:hidden mt-4 space-y-4">
-          <Link to="/" className="block text-gray-200 hover:text-gray-400">Home</Link>
-          <Link to="/about" className="block text-gray-200 hover:text-gray-400">About</Link>
-          <Link to="/services" className="block text-gray-200 hover:text-gray-400">Services</Link>
-          <Link to="/contact" className="block text-gray-200 hover:text-gray-400">Contact</Link>
-        </div>
-      )}
+  <div className="md:hidden absolute top-24 w-full bg-white bg-opacity-95  py-2 rounded-lg shadow-lg">
+    <nav className="flex flex-col items-center space-y-3">
+      <Link
+        to="/"
+        className="text-gray-500 text-md font-semibold hover:text-blue-200 transition duration-200"
+      >
+        Home
+      </Link>
+      <Link
+        to="/about"
+        className="text-gray-500  text-md font-semibold hover:text-blue-200 transition duration-200"
+      >
+        About
+      </Link>
+      <Link
+        to="/services"
+        className="text-gray-500  text-md font-semibold hover:text-blue-200 transition duration-200"
+      >
+        Services
+      </Link>
+      <Link
+        to="/contact"
+        className="text-gray-500 text-md font-semibold hover:text-blue-200 transition duration-200"
+      >
+        Contact
+      </Link>
+    </nav>
+  </div>
+)}
+  
     </nav>
   );
 };
